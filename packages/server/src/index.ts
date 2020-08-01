f1de50e51a28b3f92a752f0f594685fd9412a923
Lucas Munhoz <ln.munhoz@gmail.com>
2020-08-01 17:24:31 +0700

    Setup prisma as dependency of server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,33 @@
 import express from "express"
+import faker from "faker"
 import { CommonModule } from "@monorepo/common"
+import { PrismaClient } from "@monorepo/db"
 
 const app = express()
 const PORT = process.env.PORT || 4000
+const prisma = new PrismaClient()
+
+prisma
+   .connect()
+   .then(() => console.log("prisma connected"))
+   .catch((err) => console.log("prisma connection error"))
+
+prisma.user
+   .create({
+      data: {
+         email: faker.internet.email(),
+      },
+   })
+   .then(() => console.log("user created"))
+   .catch((err) => console.log("error creating user"))
 
 app.get("/hello", (req, res) => {
    res.status(200).send(`${CommonModule}`)
+})
+
+app.get("/users/count", async (req, res) => {
+   const usersCount = await prisma.user.count()
+   res.status(200).send(`${usersCount}`)
 })
 
 app.listen(PORT, () => {
